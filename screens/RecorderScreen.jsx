60ea3297ef29d0c9314e6b4a03fa321260e8a1f3
JoseAntonio1DAM <usr4034@salesianos-lacuesta.net>
2026-04-15 08:38:15 +0100

    feat: diseño principal de la pantalla

---
+++ screens/RecorderScreen.jsx
@@ -0,0 +1,96 @@
+import { StyleSheet, Text, View } from 'react-native'
+import { Ionicons } from '@expo/vector-icons';
+import React from 'react'
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { TouchableOpacity } from 'react-native';
+import { useState } from 'react';
+
+const RecorderScreens = () => {
+  const [isRecording, setIsRecording] = useState(false);
+
+  return (
+    <>
+   <View style={styles.container}>
+  <View style={styles.header}>
+    <Text style={styles.title}>JoseRecorder</Text>
+    <TouchableOpacity>
+      <Ionicons name="moon-sharp" size={24} color="white" />
+    </TouchableOpacity>
+  </View>
+
+  <View style={styles.recorderSection}>
+    <TouchableOpacity style={styles.recordButton} onPress={() => setIsRecording(!isRecording)}>
+      <FontAwesome name="microphone" size={32} color="white" />
+      <Text style={styles.recordButtonText}>{isRecording ? "Parar" : "Grabar"}</Text>
+    </TouchableOpacity>
+  </View>
+
+  <View style={styles.audioSection}>
+    <View style={styles.audioHeader}>
+      <Text style={styles.audioTitle}>Audios</Text>
+      <FontAwesome name="trash-o" size={24} color="white" />
+    </View>
+  </View>
+</View>
+
+      </>
+  )
+}
+
+export default RecorderScreens
+
+const styles = StyleSheet.create({
+container: {
+    flex: 1,
+    backgroundColor: '#1a1a1a',
+    paddingTop: 50,
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    marginBottom: 40,
+  },
+  title: {
+    color: 'white',
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
+  recorderSection: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  recordButton: {
+    backgroundColor: '#c0392b',
+    width: 120,
+    height: 120,
+    borderRadius: 60,
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 8,
+  },
+  recordButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  audioSection: {
+    backgroundColor: '#2a2a2a',
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    padding: 20,
+    minHeight: 250,
+  },
+  audioHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  audioTitle: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  
+})
